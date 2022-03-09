7bf3b61ef5ca47ee017b6c730bf3ae1db804de77
Eleazar Ramos Cortés <sixen25@gmail.com>
2022-03-09 20:43:44 +0100

    fin sección 20

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,14 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { PagesComponent } from './pages.component';
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-
-
 import { SharedModule } from '../shared/shared.module';
 import { ComponentsModule } from '../components/components.module';
-
-import { PagesComponent } from './pages.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { ProgressComponent } from './progress/progress.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -19,9 +16,8 @@ import { PerfilComponent } from './perfil/perfil.component';
 import { UsuariosComponent } from './mantenimientos/usuarios/usuarios.component';
 import { HospitalesComponent } from './mantenimientos/hospitales/hospitales.component';
 import { MedicosComponent } from './mantenimientos/medicos/medicos.component';
-
-import { PipesModule } from '../pipes/pipes.module';
 import { MedicoComponent } from './mantenimientos/medicos/medico.component';
+import { PipesModule } from '../pipes/pipes.module';
 import { BusquedaComponent } from './busqueda/busqueda.component';
 
 @NgModule({
